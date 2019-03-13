f61ed41fb3787c35fc475d8234f6b53f1f097366
Andre Popovitch <andre@popovit.ch>
2019-03-13 17:49:36 -0400

    done absolutely no work yet

---
+++ zoda-lang/target/rls/debug/deps/zoda_lang-0344a8cdf662ffe5.d
@@ -0,0 +1,5 @@
+c:\Users\hyper\dev\Zoda\zoda-lang\target\rls\debug\deps\zoda_lang-0344a8cdf662ffe5.rmeta: src\main.rs
+
+c:\Users\hyper\dev\Zoda\zoda-lang\target\rls\debug\deps\zoda_lang-0344a8cdf662ffe5.d: src\main.rs
+
+src\main.rs:
---
+++ zoda-lang/target/rls/debug/deps/zoda_lang-a9dcf6bad9d39b9e.d
@@ -0,0 +1,5 @@
+c:\Users\hyper\dev\Zoda\zoda-lang\target\rls\debug\deps\zoda_lang-a9dcf6bad9d39b9e.rmeta: src\main.rs
+
+c:\Users\hyper\dev\Zoda\zoda-lang\target\rls\debug\deps\zoda_lang-a9dcf6bad9d39b9e.d: src\main.rs
+
+src\main.rs:
