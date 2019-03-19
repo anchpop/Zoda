71bcd5b0324c5e1a19484577cece76242c370f5d
Andre Popovitch <andre@popovit.ch>
2019-03-19 17:53:37 -0400

    wrote down some more ideas

--- zoda-lang/src/main.rs
+++ zoda-lang/src/main.rs
@@ -1,6 +1,18 @@
-fn main() {
-    println!("Hello, world!");
-    
+#[macro_use] extern crate lalrpop_util;
+
+
+lalrpop_mod!(pub calculator2b); // synthesized by LALRPOP
+
+#[test]
+fn calculator1() {
+    assert!(calculator1::TermParser::new().parse("22").is_ok());
+    assert!(calculator1::TermParser::new().parse("(22)").is_ok());
+    assert!(calculator1::TermParser::new().parse("((((22))))").is_ok());
+    assert!(calculator1::TermParser::new().parse("((22)").is_err());
 }
 
 
+fn main() {
+    println!("{}", calculator1::TermParser::new().parse("(22").is_ok());
+    
+}
