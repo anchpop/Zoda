141ab08676c424c8b14f30b99f9f94a607f7d042
Andre Popovitch <andre@popovit.ch>
2019-04-14 00:21:04 -0400

    added convenience typedef

--- zoda-lang-hs/src/Parser.hs
+++ zoda-lang-hs/src/Parser.hs
@@ -11,30 +11,14 @@ import Debug.Trace
 parseModule
   :: String -> Either (ParseErrorBundle String Void) (Module SourcePosition)
 parseModule text =
-  evalState (runParserT moduleP "book.md" text) (ParserState 0)
+  (runParser (evalStateT moduleP (ParserState 0)) "book.md" text)
 
+moduleP :: ASTParser Module
 moduleP = empty
 
 
-parseTesting :: String -> Either (ParseErrorBundle String Void) ParserState
-parseTesting text =
-  (runParser (evalStateT testP (ParserState 0)) "book.md" text)
 
-testP :: SParser ParserState--testP :: ParserM m => m Char
-testP = do
-  (try (oneP)) <|> (try (char '2'))
-  g <- get
-  pure g
-
-oneP :: SParser Char--oneP :: ParserM m => m Char
-oneP = do
-  (ParserState i) <- get
-  put (ParserState (i + 1))
-  char '1'
-
-
-type ParserM m = (MonadParsec Void String m, MonadState ParserState m)
 data ParserState = ParserState Int deriving (Show, Read, Eq)
-type Parser = Parsec Void String
-type SParser a = StateT ParserState Parser a
+type Parser a = StateT ParserState (Parsec Void String) a
+type ASTParser a = Parser (a SourcePosition)
 data SourcePosition = SourcePosition {_filePath :: String, _sourceLineStart :: Int, _sourceColumnStart  :: Int, _sourceLineEnd :: Int, _sourceColumnEnd  :: Int} deriving (Show, Read, Eq)
