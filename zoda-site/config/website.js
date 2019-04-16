afba32fbd71fe5a4969b99a087a5163e0514a4e3
Andre Popovitch <andre@popovit.ch>
2019-04-16 19:52:07 -0400

    changed site descsription

--- zoda-site/config/website.js
+++ zoda-site/config/website.js
@@ -3,21 +3,21 @@ const tailwind = require('../tailwind')
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Cara - Gatsby Starter Portfolio', // Navigation and Site Title
-  siteTitleAlt: 'Cara', // Alternative Site title for SEO
-  siteTitleShort: 'Cara', // short_name for manifest
-  siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
-  siteUrl: 'https://cara.lekoarts.de', // Domain of your site. No trailing slash!
+  siteTitle: 'Zoda - A Programming Language for Games', // Navigation and Site Title
+  siteTitleAlt: 'Zoda', // Alternative Site title for SEO
+  siteTitleShort: 'Zoda', // short_name for manifest
+  siteHeadline: 'A new programming language built for application development', // Headline for schema.org JSONLD
+  siteUrl: 'https://zoda.dev', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logo.png', // Used for SEO and manifest
-  siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
-  author: 'LekoArts', // Author for schema.org JSONLD
+  siteDescription: 'A new programming language built for application development',
+  author: 'Andre Popovitch', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@cara', // Twitter Username
-  ogSiteName: 'cara', // Facebook Site Name
+  userTwitter: '@popovitchandre', // Twitter Username
+  ogSiteName: 'zoda', // Facebook Site Name
   ogLanguage: 'en_US', // Facebook Language
-  googleAnalyticsID: 'UA-47519312-5',
+  googleAnalyticsID: 'UA-138019148-2',
 
   // Manifest and Progress color
   themeColor: tailwind.colors.orange,
