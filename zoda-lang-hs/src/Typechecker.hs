92382c23329b7031d4ef71c86e3d50220c6ca2f0
Andre Popovitch <andre@popovit.ch>
2019-11-20 14:10:25 -0500

    use either instead of crime

--- zoda-lang-hs/src/Typechecker.hs
+++ zoda-lang-hs/src/Typechecker.hs
@@ -14,45 +14,62 @@ typecheck = undefined
 sameValue :: Env t p ph m i -> JustifiedExpression t p ph m i -> JustifiedExpression t p ph m i -> Bool
 sameValue = undefined
 
--- |Takes some arguments and a function type
+
 doTelescope :: Env t p ph m i ->  (Nominal t, Nominal p, Nominal m, Nominal i) => (NonEmpty (JustifiedExpression t p ph m i)) -> JustifiedTelescope t p ph m i ->  JustifiedExpression t p ph m i
 doTelescope context (arg :| []) (Pi ((Just (_, (a, _)), NoBind typ) :. result)) = undefined
 doTelescope context (arg :| []) (Pi ((Nothing         , NoBind typ) :. result)) = undefined
 
 
-makeTelescope :: (Nominal t, Nominal p, Nominal m, Nominal i) => Env t p ph m i -> JustifiedFunctionLiteral t p ph m i -> JustifiedTelescope t p ph m i
-makeTelescope context (LastArg ((a, t) :. e)) = Pi ((Just a, t) :. typ)
-  where typ = inferType context e 
-makeTelescope context (Arg (((i, (a, p)), NoBind t) :. scope)) = Scope ((Just (i, (a, p)), NoBind t) :. (makeTelescope context' scope))  
+makeTelescope :: (Nominal t, Nominal p, Nominal m, Nominal i) => Env t p ph m i -> JustifiedFunctionLiteral t p ph m i -> Either () (JustifiedTelescope t p ph m i)
+makeTelescope context (LastArg ((a, t) :. e)) = do
+    typ <- inferType context e 
+    pure $ Pi ((Just a, t) :. typ)
+
+makeTelescope context (Arg (((i, (a, p)), NoBind t) :. scope)) = do
+    scope' <- makeTelescope context' scope
+    pure $ Scope ((Just (i, (a, p)), NoBind t) :. scope')  
   where context' = (a, t):context
 
 extendContextWithScope context scope = undefined -- adds all the (binding:type)s from the telescope to the context
 checkScope context scope args = undefined -- takes a list of args and a telescope and checks thet all the args' types match the associated types of the telescope
 
 
-inferType :: (Nominal t, Nominal p, Nominal m, Nominal i) => Env t p ph m i -> JustifiedExpression t p ph m i -> JustifiedExpression t p ph m i
-inferType context (Annotation expr typ t p) = 
-  (checkType context expr typ) `seq` typ -- also need to check that it's a valid type!!!
-inferType context (FunctionLiteralExpression flit t p) = 
-  TArrowBinding (makeTelescope context flit) t p
-inferType context (FunctionApplicationExpression func args t p) = 
-  case inferType context func of 
-    TArrowBinding scope _ _ -> (checkScope context scope args) `seq` (getOutputOfScope scope) 
-    _                       -> error "type error!"
+inferType :: (Nominal t, Nominal p, Nominal m, Nominal i) => Env t p ph m i -> JustifiedExpression t p ph m i -> Either () (JustifiedExpression t p ph m i)
+inferType context (Annotation expr typ t p) = checkType context expr typ *> pure typ -- also need to check that it's a valid type!!!
+inferType context (FunctionLiteralExpression flit t p) = do 
+  scope <- makeTelescope context flit
+  pure $ TArrowBinding scope t p
+inferType context (FunctionApplicationExpression func args t p) = do
+  funcType <- inferType context func 
+  case funcType of 
+    TArrowBinding scope _ _ -> do
+                                checkScope context scope args
+                                pure $ getOutputOfScope scope
+    _                       -> Left ()
 inferType context (LambdaVariable (_, x) _ _) = 
   case x `lookup` context of
-    Just t -> t
-    _      -> error "value not in context, this is bad!"
-inferType context (AddExpression e1 e2 t p) = 
-  (checkType context e1 (NatTypeExpression t p)) `seq` (checkType context e2 (NatTypeExpression t p)) `seq` (NatTypeExpression t p)
-inferType context (NumberLiteral _ t p) = NatTypeExpression t p
+    Just t -> pure t
+    _      -> Left ()
+inferType context (AddExpression e1 e2 t p) = do 
+  checkType context e1 (NatTypeExpression t p) 
+  checkType context e2 (NatTypeExpression t p) 
+  pure $ NatTypeExpression t p
+inferType context (NumberLiteral _ t p) = pure $ NatTypeExpression t p
 inferType context (ParenthesizedExpression e _ _) = inferType context e
 
-checkType :: (Nominal t, Nominal p, Nominal m, Nominal i) => Env t p ph m i -> JustifiedExpression t p ph m i -> JustifiedExpression t p ph m i -> ()
+checkType :: (Nominal t, Nominal p, Nominal m, Nominal i) => Env t p ph m i -> JustifiedExpression t p ph m i -> JustifiedExpression t p ph m i -> Either () ()
+-- Typing rule for lambda abstraction
+--       Gamma, x:t  |-  e : s
+-- ------------------------------------
+--   Gamma |-  Lam (x.e) : Pi t (x.s)
+-- note that the two `x`s in the binders have to actually be the "same" to correctly type it
+-- since `x` might be mentioned in both `e` and `s`
+-- the `merge` function in nominal is used for this
 checkType context (FunctionLiteralExpression flit _ _) shouldBe = 
   case shouldBe of 
-    TArrowBinding scope _ _ -> checkType (extendContextWithScope context scope) (getBodyOfFlit flit) (getOutputOfScope scope)
-    
+    TArrowBinding scope _ _ -> let (megedScope, mergedFlit) = undefined  
+                               in checkType (extendContextWithScope context scope) (getBodyOfFlit flit) (getOutputOfScope scope)
 checkType context (ParenthesizedExpression e _ _) shouldBe = checkType context e shouldBe
-checkType context other shouldBe = if sameValue context t shouldBe then () else error "type error!" 
-    where t = inferType context other
+checkType context toCheck shouldBe = do
+  toCheckType <- inferType context toCheck
+  if sameValue context toCheckType shouldBe then Right () else Left ()
